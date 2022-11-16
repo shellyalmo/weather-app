5aed575c9dbcab002d4950b0758263b333b510f7
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2022-11-16 12:21:19 +0200

    add local datetime and search bar

--- src/index.js
+++ src/index.js
@@ -13,10 +13,21 @@ function App() {
     .then(data => setWeatherData(data))
   },[]);
 
+
   return (
     <div className='screen'>
+      <div className='date-box'>
+       { weatherData && weatherData.current_condition[0].localObsDateTime}
+      </div>
       In {weatherData && (weatherData.nearest_area[0].country[0].value)} the current temperature is {weatherData && (weatherData.current_condition[0].temp_C)}°C
+      <div className='search-bar'>
+        <input
+        type="text"
+        placeholder='city name...'
+        />
+      </div>
     </div>
+    
   );
 }
 
