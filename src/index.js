d88a8be1bcd17db77550a71f35ffd25fceff3bba
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2022-11-20 17:53:44 +0200

    fix loading errors and add loading alerts

--- src/index.js
+++ src/index.js
@@ -1,33 +1,56 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
-
   let [weatherData, setWeatherData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
-    fetch("https://wttr.in/?lang=en&format=j1")
-    .then(response => response.json())
-    .then(data => setWeatherData(data))
-  },[]);
+    setIsLoading(true);
+    setHasError(false);
 
+    fetch("https://wttr.in/?lang=en&format=j1")
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setHasError(true);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
-    <div className='screen'>
-      <div className='date-box'>
-       { weatherData && weatherData.current_condition[0].localObsDateTime}
-      </div>
-      In {weatherData && (weatherData.nearest_area[0].country[0].value)} the current temperature is {weatherData && (weatherData.current_condition[0].temp_C)}°C
-      <div className='search-bar'>
-        <input
-        type="text"
-        placeholder='city name...'
-        />
-      </div>
+    <div>
+      {hasError && <p>Something went wrong.</p>}
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <div className="screen">
+          <div className="date-box">
+            {hasError && <p>Something went wrong.</p>}
+            Forecast time:{" "}
+            {weatherData && weatherData.current_condition[0].localObsDateTime}
+          </div>
+          <div>
+            In {weatherData && weatherData.nearest_area[0].country[0].value} the
+            current temperature is{" "}
+            {weatherData && weatherData.current_condition[0].temp_C}°C Weather
+            is:{" "}
+            {weatherData &&
+              weatherData.current_condition[0].weatherDesc[0].value}
+          </div>
+          {/* <div className="search-bar">
+            <input type="search" placeholder="city name..." />
+            <button>Show weather</button>
+          </div> */}
+          {/* <div className="weather-by-city-box">city weather</div> */}
+        </div>
+      )}
     </div>
-    
   );
 }
 
